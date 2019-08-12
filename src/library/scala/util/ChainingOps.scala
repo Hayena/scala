f10ff86685992d3bbdd2fcff24ff4a6d015c9fc4
Donovan <Hayena@users.noreply.github.com>
2019-08-12 08:03:17 +0200

    Added alias for pipe operator
    Within other languages (F#, Elixer, JavaScript) the pipe operator is defined as `|>`. I think

--- src/library/scala/util/ChainingOps.scala
+++ src/library/scala/util/ChainingOps.scala
@@ -60,4 +60,26 @@ final class ChainingOps[A](private val self: A) extends AnyVal {
     *                `f` to this value.
     */
   def pipe[B](f: A => B): B = f(self)
+  
+  /** Converts the value by applying the function `f`.
+    *
+    * {{{
+    *   scala> import scala.util.chaining._
+    *
+    *   scala> val times6 = (_: Int) * 6
+    *   times6: Int => Int = $$Lambda$2023/975629453@17143b3b
+    *
+    *   scala> val i = (1 - 2 - 3) |> times6 |> scala.math.abs
+    *   i: Int = 24
+    * }}}
+    *
+    * Note: `(1 - 2 - 3) |> times6` may have a small amount of overhead at
+    * runtime compared to the equivalent  `{ val temp = 1 - 2 - 3; times6(temp) }`.
+    *
+    *  @param f      the function to apply to the value.
+    *  @tparam B     the result type of the function `f`.
+    *  @return       a new value resulting from applying the given function
+    *                `f` to this value.
+    */
+  def |>[B](f: A => B): F = pipe(f)
 }
